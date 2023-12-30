d9dafd1919c0fb5628e94c8e66f39d92a3c903c5
swagolo1 <solomonserban@gmail.com>
2023-12-30 13:17:55 +0200

    small edits

--- index.js
+++ index.js
@@ -6,23 +6,55 @@ canvas.height = innerHeight
 
 class Player {
     constructor(){
-        this.position = {
-            x: 200,
-            y: 200
-        }
         this.velocity = {
             x: 0,
             y: 0
         }
-        // this.image
-        this.width = 100
-        this.height = 100
+        const image = new Image()
+        image.src = './img/spaceship.png'
+        image.onload = () =>{
+            const scale = 0.15
+            this.image = image
+            this.width = image.width * scale
+            this.height = image.height * scale
+            this.position = {
+                x: canvas.width / 2 - this.width / 2,
+                y: canvas.height - this.height - 20
+            }
+        }
+
     }
     draw(){
-        c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+        // c.fillStyle = 'red'
+        // c.fillRect(this.position.x, this.position.y, this.width, this.height)
+        if(this.image){
+
+            c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
+        }
     }
 }
 
 const player = new Player()
 player.draw()
+
+function animate(){
+    requestAnimationFrame(animate)
+    c.fillStyle = 'black'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    player.draw()
+}
+
+animate()
+
+addEventListener( 'keydown', ({key})=> {
+    // console.log(key)
+    switch (key){
+        case 'a':
+            console.log('left')
+            break
+        case 'd':
+            console.log('right')
+            break
+        
+    }
+})
